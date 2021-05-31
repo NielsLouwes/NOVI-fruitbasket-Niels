09b9c8d380c3c08a4d853d2fad7b06c38cc24544
NielsLouwes <nielslouwes45@gmail.com>
2021-05-31 21:42:32 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useState} from "react";
 import { useForm } from 'react-hook-form';
 
+
 function App() {
 
   const [count,setCount] = useState(0);
@@ -13,15 +14,22 @@ function App() {
   const resetCounts = () => {
     setCount ( 0), setCountTwo(0), setCountThree(0), setCountFour(0);
   }
+  const [succes, setSucces] = useState(false);
+  const { register, handleSubmit, formState: {errors}, } = useForm();
+  //function onSubmit takes the data from the form and logs it
+  const onSubmit = data => console.log(data);
 
-  return (
+  console.log(errors);
+
+
+    return (
 
   <section>
     <h1>Fruitmand bezorgservice</h1>
     <h2>Je mag toch niet naar buiten</h2>
 
     <section className={"form-section"}>
-      <form className={"form"} action="#">
+      <form className={"form-one"}>
 
         <div className={"fruit-items"}>
           <label htmlFor="strawberry"> Aardbeien</label>
@@ -40,9 +48,13 @@ function App() {
               name="bananas"
               id="bananas"
               value={countTwo}
-              onClick={()=> setCountTwo(countTwo + 1)}> +</button>
+              onClick={()=> setCountTwo(countTwo + 1)}>
+            +
+          </button>
             <p>{countTwo}</p>
-          <button onClick={()=> setCountTwo(countTwo - 1)}>-</button>
+          <button onClick={()=> setCountTwo(countTwo - 1)}>
+            -
+          </button>
         </div>
 
         <div className={"fruit-items"}>
@@ -76,32 +88,48 @@ function App() {
       </form>
 
 
-            // react hook form
+
       <section>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           <div>
           <label htmlFor="firstname">Voornaam</label>
-          <input id={"firstname"} type="text"/>
+          <input {...register("firstname", {required: { value: true, message: "Dit veld is verplicht."}, maxLength: 20 })}
+                 id={"firstname"}
+                 type="text"
+          />
+            {errors.name && <p>{errors.name.message}</p>}
           </div>
 
           <div>
             <label htmlFor="lastname">Achternaam</label>
-            <input id={"lastname"} type="text"/>
+            <input {...register("lastname", {pattern: /^[A-Za-z]+$/i })}
+                   id={"lastname"}
+                   type="text"/>
           </div>
 
           <div>
             <label htmlFor="age">Leeftijd</label>
-            <input id={"age"}   type="text"/>
+            <input {...register("age", {required: true, min: 18, max:99 })}
+                   id={"age"}
+                   type="text"/>
           </div>
 
           <div>
             <label htmlFor="postalcode">Postcode</label>
-            <input id={"postalcode"}  type="text"/>
+            <input {...register("postalcode", {required: true, message: "Dit veld is verplicht"})}
+                   id={"postalcode"}
+                   type="text"
+            />
+            {errors.postalcode && <p>{errors.postalcode.message}</p>}
+
           </div>
           
           <div>
             <label htmlFor="frequency">Bezorgfrequentie</label>
-            <select name="delivery" id="delivery">
+            <select {...register("frequency", {required: true})}
+                name="delivery"
+                id="delivery"
+            >
               <option value="weekly">Iedere week</option>
               <option value="biWeekly">Om de week</option>
               <option value="monthly">Iedere maand</option>
@@ -109,15 +137,25 @@ function App() {
             </select>
           </div>
             <label htmlFor="commentbox">Opmerking</label>
-            <textarea name="commentbox" id="commentbox" cols="70" rows="8"></textarea>
+            <textarea
+                name="commentbox"
+                id="commentbox"
+                cols="70"
+                rows="8">
 
+
+            </textarea>
+
+
+
+          <input type="submit" disabled={errors.name || errors.email ||errors.xp}/>
 
         </form>
 
       </section>
 
 
-    </section>
+
   </section>
 
   );
